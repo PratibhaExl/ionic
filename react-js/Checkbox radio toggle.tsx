a16c74fc225edf67da6228fccaea24daae91e2df
Pratibha <150898183+PratibhaExl@users.noreply.github.com>
2025-03-10 15:13:51 +0530

    Update Checkbox radio toggle.tsx

--- react-js/Checkbox radio toggle.tsx
+++ react-js/Checkbox radio toggle.tsx
@@ -1,5 +1,67 @@
 
 
+<Controller
+  name="isToggleEnabled"
+  control={control}
+  render={({ field }) => (
+    <Switch
+      checked={field.value}
+      onChange={(e) => setValue("isToggleEnabled", e.target.checked)}
+    />
+  )}
+/>
+
+
+<Controller
+  name="selectedRadio"
+  control={control}
+  render={({ field }) => (
+    <RadioGroup
+      {...field}
+      onChange={(e) => setValue("selectedRadio", e.target.value)}
+    >
+      <FormControlLabel value="option1" control={<Radio />} label="Option 1" />
+      <FormControlLabel value="option2" control={<Radio />} label="Option 2" />
+    </RadioGroup>
+  )}
+/>
+
+<Controller
+  name="selectedCheckboxes"
+  control={control}
+  render={({ field }) => (
+    <FormGroup>
+      {["option1", "option2", "option3"].map((option) => (
+        <FormControlLabel
+          key={option}
+          control={
+            <Checkbox
+              checked={field.value.includes(option)}
+              onChange={(e) => {
+                const updatedValues = e.target.checked
+                  ? [...field.value, option]
+                  : field.value.filter((v: string) => v !== option);
+                setValue("selectedCheckboxes", updatedValues);
+              }}
+            />
+          }
+          label={option}
+        />
+      ))}
+    </FormGroup>
+  )}
+/>
+
+
+
+
+
+/////
+
+
+
+
+
 {fieldConfig.type === "toggle" && (
   <Grid item xs={12} className="dynamic-form-field">
     <FormLabel className="field-label">
